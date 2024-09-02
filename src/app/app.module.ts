2d27dcfff1a870dbb6250f4ec9400a26cfa6aa65
Andrew <anperez@unividamedicalcenters.com>
2024-09-02 00:53:15 -0400

    gifs app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GifsModule,
+    SharedModule,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()  // Provide HttpClient using the new method
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import {  Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+})
+export class LazyImageComponent  implements OnInit{
+
+
+ @Input()
+ public url!: string;
+
+@Input()
+ public alt: string = '';
+
+ public hasLoaded:boolean =false;
+
+ onLoad(){
+  setTimeout(() => {
+  this.hasLoaded = true;
+  }, 1000);
+
+ }
+ ngOnInit(): void {
+  if(!this.url)
+   throw new Error('URL property es required.');
+ }
+
+ }
